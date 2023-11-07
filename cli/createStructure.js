cbf2d1170e375b88eb32354b1b80d1b87b21e149
Robert Schnüll <mail@robertschnuell.de>
2023-11-07 15:49:29 +0100

    Added '-r' flag for /cli/createStructure.js
    Added a conditional check for the-r flag to output only the id of the first element in the created data array.

--- cli/createStructure.js
+++ cli/createStructure.js
@@ -23,6 +23,9 @@ async function main() {
     const token = process.argv.findIndex((i) => i === '-t') > 0 ? process.argv[process.argv.findIndex((i) => i === '-t')+1] : null;
     const baseurl = process.argv.findIndex((i) => i === '-b') > 0 ? removeTrailingSlash(process.argv[process.argv.findIndex((i) => i === '-b')+1]) : null;
     const homeserver = process.argv.findIndex((i) => i === '-s') > 0 ? process.argv[process.argv.findIndex((i) => i === '-s')+1] : null;
+
+    const outputOnlyRootId = process.argv.findIndex((i) => i === '-r') > 0;
+
     const help = process.argv.findIndex((i) => i === '-h') > 0;
 
     // Display help message and exit if the -h flag is provided
@@ -75,6 +78,13 @@ async function main() {
 
     // Assign spaces based on the created spaces
     await assignSpaces(data, matrix);
+
+    // If outputOnlyRootId is true, write the id of the first element in the data array to stdout.
+    // If the data array is empty, write an empty string to stdout. Then exit the process.
+    if (outputOnlyRootId) {
+        data.length> 0 ? process.stdout.write(data[0].id) : process.stdout.write('');
+        process.exit();
+    }
 
     // Output the generated spaces as an array to STDOUT
     process.stdout.write(getJsonStringFromGeneratedData(data));
@@ -243,6 +253,8 @@ Command-line arguments supported by \`node ./cli/createStructure.js\`:
         server_name of the matrix server, e.g. "medienhaus.dev" (required)
     -h
         print this help message
+    -r
+        prints out only the created rootId as an output to STDOUT instead of the full json array as an list
 
 `,
     );
